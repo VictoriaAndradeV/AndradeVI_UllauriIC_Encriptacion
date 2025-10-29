992ebb17bf51b21ad484059e4e53bfff53ec4347
Victoria Andrade <vandradev2@est.ups.edu.ec>
2025-10-28 19:20:11 -0500

    Cambios en el primer ejercicio

--- src/main/java/org/example/EncripPorDesplazamientoS.java
+++ src/main/java/org/example/EncripPorDesplazamientoS.java
@@ -10,6 +10,8 @@ public class EncripPorDesplazamientoS implements AlgoritmoEncriptador<String>{
     public String encriptar(String mensaje, int desplazamiento) {
         //variable que almacena el nuevo codigo de cada letra, ya que se le suma su desplazamiento
         int codigoNuevo;
+        //variable que transforma las letras pasadas a mayuscula
+        char letraMayus;
         //variable tipo char que almacena la nueva letra, ya encriptada
         char nuevaLetra;
         //variable que guarda el mensaje final encriptado
@@ -18,15 +20,34 @@ public class EncripPorDesplazamientoS implements AlgoritmoEncriptador<String>{
         System.out.printf(" Letra Original \t Desplazamiento(+%d)", desplazamiento);
         System.out.println();
 
+        /*variable que, si tenemos un desplazamiento de 57 por ejemplo, no recorre las
+        26 letras del abecedario dos veces, sino que obtiene el mod (restante)
+        de la division 57/26, restante = 5, entonces recorre unicamente 5 letras
+         */
+        int desplazamientoEficaz = desplazamiento % 26;
+
         for(int i = 0; i < mensaje.length(); i++){
+            //variable que toma letra por letra del mensaje
+            char letra = mensaje.charAt(i);
+            //forza que la letra sea mayuscula
+            letraMayus = Character.toUpperCase(letra);
 
-            //obtiene el codigo de cada letra del String y SUMA el desplazamiento
-            //para encriptar el mensaje
-            codigoNuevo = (int) mensaje.charAt(i) + desplazamiento;
+            if(letraMayus >= 'A' && letraMayus <= 'Z'){
+                //variable que convierte la letra en un numero del 0 al 25
+                //sirve cuando colocamos la letra Z y haga el respectivo desplazamiento, contando desde A
+                int posicion = letraMayus - 'A';
 
-            //obtiene la nueva letra con el desplazamiento correspondiente
-            nuevaLetra = (char) codigoNuevo;
-            System.out.printf("%-20c %-20c\n", mensaje.charAt(i), nuevaLetra);
+                int posNueva = (posicion + desplazamientoEficaz) % 26;
+                //nos da el codigo ASCII aplicado el desplazamiento
+                codigoNuevo = 'A' + posNueva;
+
+                //obtiene la nueva letra con el desplazamiento correspondiente
+                nuevaLetra = (char) codigoNuevo;
+                System.out.printf("%-20c %-20c\n", mensaje.charAt(i), nuevaLetra);
+            }else{
+                nuevaLetra = letra;
+                System.out.printf("%-20c %-20c\n", mensaje.charAt(i), nuevaLetra);
+            }
 
             //guarda en la variable mensajeEncriptado, cada nueva letra encriptada
             mensajeEncriptado += nuevaLetra;
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("\n ALGORITMO POR DESPLAZAMIENTO \n");
         System.out.println("1- Datos de entrada");
 
-        String cadenaOriginal = "HELLO";
+        String cadenaOriginal = "ZAPATO";
         System.out.println("Texto original: \t\t" + "\"" +cadenaOriginal +"\"");
 
         int desplazamiento = 3;
